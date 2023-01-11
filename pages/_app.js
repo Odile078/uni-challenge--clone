ebf332c22585f3ce33a02085e80fd12406fb4ec7
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-01-11 08:21:19 +0200

    chore: Set up tailwind css and font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import localFont from "@next/font/local";
+const univers = localFont({
+  src: [
+    {
+      path: "../public/fonts/univers1.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/univers2.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/univers3.woff2",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+  variable: "--font-univers",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${univers.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+// tailwind.config.js
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandBlue: {
+          DEFAULT: "#004191",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-univers)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
